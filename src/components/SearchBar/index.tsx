2d02000073271b9002d34204a84ca46d9a08e1ee
dawnheee <sherlock130815@gmail.com>
2023-07-17 20:34:05 +0900

    [feat] #1 search component / debounce

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+interface SearchBarProps {
+  setLetters: (value: string) => void;
+}
 
-function SearchBar() {
-  return <div>SearchBar</div>;
+function SearchBar({ setLetters }: SearchBarProps) {
+  const inputChangeHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    setLetters(event.currentTarget.value);
+  };
+
+  return (
+    <>
+      <h4>SearchBar</h4>
+      <input onInput={inputChangeHandler} />
+    </>
+  );
 }
 
 export default SearchBar;
