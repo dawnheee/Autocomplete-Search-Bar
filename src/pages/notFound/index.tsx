399ddcfeb12e93aeab442cad23f9862ef64d686a
dawnheee <sherlock130815@gmail.com>
2023-07-19 21:29:17 +0900

    #10 404페이지

--- src/pages/notFound/index.tsx
+++ src/pages/notFound/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <p>The requested page could not be found.</p>
+    </div>
+  );
 }
 
 export default NotFound;
